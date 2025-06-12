37b7f12dc54073de7882479b4cfa5bfcfe14ab36
Felipe Heredia <297297fi365@ort.edu.uy>
2025-06-12 20:12:54 -0300

    feat: crear TodoItemsModule e integrar con AppModule

---
+++ src/todo_items/todo_items.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TodoItemsController } from './todo_items.controller';
+import { TodoItemsService } from './todo_items.service';
+
+@Module({
+  controllers: [TodoItemsController],
+  providers: [TodoItemsService],
+  exports: [TodoItemsService], // Exportar para que el servidor MCP pueda usarlo
+})
+export class TodoItemsModule {} 
